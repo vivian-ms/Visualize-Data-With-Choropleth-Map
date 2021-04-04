37737057f1d29816c3c27031a8e73061fe26687d
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-04-03 17:56:17 -0700

    Added function to append svg element to #svg_container

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const urls = {
   education: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json',
   county: 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
 };
+const w = 900;
+const h = 500;
+const padding = 100;
 
 
 window.onload = () => {
@@ -10,10 +13,20 @@ window.onload = () => {
     fetch(urls.county)
   ]).then(responses => Promise.all(responses.map(response => response.json())))
     .then(data => {
-      console.log(data);
+      createCanvas(data);
     })
     .catch(err => {
       console.log(`Error: ${err}`);
     });
 
 };  // End window.onload()
+
+
+function createCanvas(data) {
+  let svg = d3.select('#svg_container')
+              .append('svg')
+              .attr('width', w + 2 * padding)
+              .attr('height', h + 2 * padding)
+              .append('g')
+              .attr('transform', `translate(${padding}, ${padding / 10})`);
+}  // End createCanvas()
