ed8697f933ffa5b5ba7d624cc5d51d9e78c8b0e8
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-04-03 18:20:39 -0700

    Added function to draw map

--- script.js
+++ script.js
@@ -29,4 +29,25 @@ function createCanvas(data) {
               .attr('height', h + 2 * padding)
               .append('g')
               .attr('transform', `translate(${padding}, ${padding / 10})`);
+
+  createMap(svg, data);
 }  // End createCanvas()
+
+
+function createMap(svg, data) {
+  let path = d3.geoPath();
+
+  svg.selectAll('path')
+     .data(topojson.feature(data[1], data[1].objects.counties).features)
+     .enter()
+     .append('path')
+     .attr('d', path)
+     .attr('data-fips', d => d.id)
+     .attr('data-education', d => data[0].find(obj => obj.fips === d.id).bachelorsOrHigher)
+     .classed('county', true);
+
+  svg.append('path')
+     .datum(topojson.mesh(data[1], data[1].objects.states, (a, b) => a !== b))
+     .attr('d', path)
+     .classed('state', true);
+}  // End createMap()
